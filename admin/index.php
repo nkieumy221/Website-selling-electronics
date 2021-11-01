8a2affdd2d88696752e60c9e42acabe738383716
nkieumy221 <nkieumy221@gmail.com>
2021-11-01 21:42:05 +0700

    admin index

---
+++ admin/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/grid.css">
+    <link rel="stylesheet" href="./assets/css/frame.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- <meta http-equiv="refresh" content="5"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+</head>
+<body>
+    <div class="main">
+        <div class="header">
+            <div class="header__left">
+                <div class="header__logo">
+                    <img src="../assets/img/foxu.png" alt="" class="header__logo-img">
+                </div>
+                <div class="header__search">                
+                    <i class="fas fa-search icon"></i>
+                    <input class="input-field" 
+                           type="text" 
+                           placeholder="Tìm kiếm..."
+                           name="search">
+                </div>
+            </div>
+            <div class="header__right">
+                <div class="user__name">
+                    Hello <b>My</b>
+                </div>
+                <div class="logout-btn">
+                    <a href=""class="logout__link">
+                        Đăng xuất
+                    </a>
+                </div>
+            </div>
+        </div>
+        <?php include('./inc/sliderbar.php'); ?>
+        <div class="main__content">
+            <div class="slider__bar">
+                
+                <ul class="slider__menu">
+                    <li class="slider__item">
+                        <a href="" class="slider__item-link slider__item-link--active">
+                            Tổng quan
+                        </a>
+                    </li>
+                    <li class="silder__item">
+                        <a href="" class="slider__item-link">
+                            Quản lý danh mục
+                        </a>
+                    </li>
+                    <li class="silder__item">
+                        <a href="" class="slider__item-link">
+                            Quản lý hàng hóa
+                        </a>
+                    </li>
+                    <li class="silder__item">
+                        <a href="" class="slider__item-link">
+                            Quản lý đơn hàng
+                        </a>
+                    </li>
+                    <li class="silder__item">
+                        <a href="" class="slider__item-link">
+                            Quản lý tài khoản 
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="page_content">
+            1 07:52 GMT+7
+87
+5
+
+Lưu
+TTO - Theo "quy luật" chung của vắc xin phòng bệnh, cụ thể là vắc xin COVID-19, sau khi tiêm đủ 2 mũi vắc xin cơ bản cần tiêm mũi 3 để củng cố kháng thể chống COVID-19 vì hiệu quả vắc xin có thể giảm dần theo thời gian.
+Dự kiến cuối 2021, TP.HCM tiêm vắc xin mũi 3 cho nhóm nguy cơ cao, lực lượng tuyến đầu
+Toàn cầu đã tiêm hơn 6 tỉ liều vắc xin, còn 3 nước chưa tiêm mũi nào
+Không cần tiêm mũi 3 Moderna vì hiệu quả mũi 2 còn cao
+Mũi 3 vắc xin COVID-19 nào phù hợp với 2 mũi đầu? - Ảnh 1.
+TP.HCM đã tính đến tiêm mũi 3 tăng cường kháng thể - Ảnh: D.PHAN
+
+Sở Y tế TP.HCM vừa cho biết đang đề xuất UBND TP tiêm vắc xin mũi 3 cho lực lượng tuyến đầu chống dịch, người có nguy cơ cao vào hai tháng cuối năm 2021. Sang năm 2022, dự kiến tiêm mũi 3 và mũi 4 cho người đủ thời gian theo quy định của Bộ Y tế.
+
+Bao giờ tiêm lại cho người dân?
+
+Từ ngày 8-3-2021, TP.HCM bắt đầu chiến dịch tiêm vắc xin cho lực lượng tuyến đầu chống dịch là những nhân viên y tế Bệnh viện Bệnh nhiệt đới TP.HCM. 
+
+Theo lãnh đạo Sở Y tế TP, đến nay đã có khoảng 75.400 người là lực lượng chống dịch và nhân viên y tế của TP và lực lượng được huy động trên cả nước được tiêm đủ vắc xin (riêng TP.HCM có 55.000 nhân viên y tế tham gia chống dịch đợt 4).
+
+Còn đối với người cao tuổi, mắc bệnh nền... đã bắt đầu tiêm vắc xin mũi 1 vào những ngày cuối tháng 7. Như vậy, tính đến nay, thời gian bắt đầu tiêm mũi 1 cho những người đầu tiên của lực lượng tuyến đầu đã khoảng 8 tháng; còn với người cao tuổi mắc bệnh nền khoảng 3 tháng.
+
+Phó GS Đỗ Văn Dũng - trưởng khoa y tế công cộng, nguyên phó hiệu trưởng Trường ĐH Y dược TP.HCM - cho hay miễn dịch chống lại COVID-19 bao gồm kháng thể và tế bào T và B có trí nhớ miễn dịch. 
+
+Theo đó, kháng thể đóng vai trò quan trọng trong bảo vệ chúng ta không bị mắc COVID-19 có triệu chứng, còn các tế bào T và B miễn dịch có vai trò bảo vệ không bị COVID-19 nặng hoặc tử vong.
+
+Dựa trên số liệu về kháng thể ở người đã được tiêm vắc xin, tiến sĩ Miles Davenport (Đại học New South Wales, Sydney, Úc) ước tính kháng thể chống COVID-19 sẽ giảm đi phân nửa sau 108 ngày (khoảng 15 tuần). Vì vậy giả sử hiệu lực vắc xin chống lại mắc COVID-19 có triệu chứng ở thời điểm ban đầu là 90% thì hiệu lực này sẽ chỉ còn 70% sau 6 tháng.
+
+Việc chọn lựa vắc xin để tiêm chéo có thể không phải là vấn đề lớn. Ngoài ra do việc nghiên cứu về tiêm chéo cũng đơn giản, chỉ cần đánh giá tính an toàn và thay đổi nồng độ kháng thể nên có thể vừa triển khai tiêm mũi tiêm tăng cường vừa đánh giá.
+
+PGS ĐỖ VĂN DŨNG
+
+Tiêm mũi 3 là cần thiết
+
+Theo ông Dũng, việc suy giảm miễn dịch chống lại người nhiễm COVID-19 có triệu chứng (dù không tăng nguy cơ bệnh nặng hay tử vong) sẽ làm tăng nguy cơ truyền nhiễm COVID-19 trong các cơ sở y tế và cộng đồng. Vì vậy việc tiêm vắc xin mũi 3 cho những người này là cần thiết.
+
+Ngoài ra, ở người cao tuổi, người mắc bệnh nền hoặc sử dụng vắc xin có hiệu lực thấp thì do khả năng duy trì miễn dịch có thể kém hơn nên hiệu lực vắc xin phòng diễn tiến nặng khi mắc COVID-19 cũng giảm theo thời gian.
+
+Ông Dũng dẫn nghiên cứu trên người cao tuổi ở Israel trong vụ dịch vào tháng 7-2021 cho thấy người được tiêm vắc xin vào tháng 1 có nguy cơ bị bệnh nặng gấp đôi so với người được tiêm gần đây. Điều này cho thấy việc tiêm mũi 3 ở người cao tuổi sau 4 đến 6 tháng là cần thiết.
+
+Tuy nhiên, ông Dũng lưu ý việc tiêm mũi tăng cường cũng nên quan tâm đến tính "sẵn có". 
+
+"Nếu ở nhiều địa phương còn nhiều người cao tuổi chưa được tiêm mũi 2 thì nên ưu tiên phân bổ vắc xin cho những người này trước khi tiến hành tiêm mũi 3 cho người cao tuổi tại TP.HCM", ông Dũng nói.
+
+Trong Hội nghị sơ kết công tác phòng chống dịch COVID-19 của ngành y tế TP.HCM trong đợt dịch vừa qua, tiến sĩ Nguyễn Văn Vĩnh Châu - phó giám đốc Sở Y tế TP - nhấn mạnh tiêm vắc xin phòng COVID-19 là chiến lược lâu dài và hàng đầu trong công tác phòng chống dịch COVID-19 của TP.
+
+"Phải đảm bảo độ bao phủ vắc xin từng người dân trong từng độ tuổi theo quy định, không chỉ dựa vào số liệu thống kê. Ưu tiên tiêm vắc xin cho người có nguy cơ cao như thai phụ, người trên 50 tuổi, béo phì và lực lượng tuyến đầu trong công tác phòng chống dịch" - tiến sĩ Vĩnh Châu cho biết.
+
+Loại vắc xin mũi 3 nào phù hợp với 2 mũi trước?
+
+Trước thắc mắc của nhiều người khi tiêm vắc xin mũi 3 có cần căn cứ vào loại vắc xin đã tiêm 2 mũi trước đó, ông Dũng cho hay theo lý luận khoa học, loại mũi tiêm tăng cường nào là phù hợp thì phải dựa trên nghiên cứu thực nghiệm.
+
+Cụ thể:
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
